5d4a0f4f3462ed5848006f482eb9a31ad112cdcd
maniram <maniramyadav786@gmail.com>
2025-07-08 19:55:29 +0530

    db setup added

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const connectDB = require('./db');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
