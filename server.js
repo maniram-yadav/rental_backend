54b28a835cf77b5babc315b20709fbbf216083c4
maniram <maniramyadav786@gmail.com>
2025-07-08 15:00:26 +0530

    rental backend initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
